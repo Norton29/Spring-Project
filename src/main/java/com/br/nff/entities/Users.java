7813cffa907365fa551c78638081352b9fc054fc
Norton29 <norton290194@gmail.com>
2022-02-22 02:53:55 -0300

    Order, Instant

--- src/main/java/com/br/nff/entities/Users.java
+++ src/main/java/com/br/nff/entities/Users.java
@@ -1,18 +1,23 @@
 package com.br.nff.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Users implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,9 +25,13 @@ public class Users implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public Users() {
-		
+
 	}
 
 	public Users(Long id, String name, String email, String phone, String password) {
@@ -74,6 +83,10 @@ public class Users implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -90,6 +103,5 @@ public class Users implements Serializable {
 		Users other = (Users) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
