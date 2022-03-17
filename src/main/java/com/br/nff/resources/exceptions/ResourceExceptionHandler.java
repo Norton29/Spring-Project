4d5379b5b5dbd879035f4ae630694b7481901436
Norton29 <norton290194@gmail.com>
2022-03-17 00:10:38 -0300

    Exception handling - delete

--- src/main/java/com/br/nff/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/br/nff/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.br.nff.services.exceptions.DatabaseException;
 import com.br.nff.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -23,4 +24,14 @@ public class ResourceExceptionHandler {
 		
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
+		String error = "Data Base error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
 }
+	
